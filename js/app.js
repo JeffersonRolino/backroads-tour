9df1f0e19c44b816cee8c215b8f68cbc85806d31
jeffersonrolino <jefferson.rolino@gmail.com>
2025-03-18 15:48:18 -0300

    feat: add navbar toggle

--- js/app.js
+++ js/app.js
@@ -3,14 +3,14 @@
 const date = (document.getElementById("date").innerHTML =
     new Date().getFullYear());
 
-// // ********** nav toggle ************
-// // select button and links
-// const navBtn = document.getElementById("nav-toggle");
-// const links = document.getElementById("nav-links");
-// // add event listener
-// navBtn.addEventListener("click", () => {
-//     links.classList.toggle("show-links");
-// });
+// ********** nav toggle ************
+// select button and links
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+// add event listener
+navBtn.addEventListener("click", () => {
+    links.classList.toggle("show-links");
+});
 
 // // ********** smooth scroll ************
 // // select links
